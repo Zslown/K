20072eb21b6bb74e8e31fd1e6cb0f9a2f3a6709e
Zslown <122195171+Zslown@users.noreply.github.com>
2023-11-28 23:12:09 +0800

    Add files via upload

---
+++ findresult/findresult.js
@@ -0,0 +1,137 @@
+// pages/mainpage/mainpage.js
+const app = getApp();
+Page({
+  data: {
+    isNightMode : app.globalData.isNightMode ,
+    titleSrc : "https://ooo.0x0.ooo/2023/11/14/OeE5OL.png",
+    logoSrc :"../img/logo.png",
+    daymodeSrc :"https://ooo.0x0.ooo/2023/11/14/OeL3zs.png",
+    nightmodeSrc :"https://ooo.0x0.ooo/2023/11/14/OeLCPK.png",
+    historySrc: "https://ooo.0x0.ooo/2023/11/17/Oefdfr.png",
+    searchSrc :"https://ooo.0x0.ooo/2023/11/14/OeETLX.png",
+    searchBoxSrc:"https://ooo.0x0.ooo/2023/11/17/OelqHS.png",
+    backSrc:"https://ooo.0x0.ooo/2023/11/17/Oel5XN.png",
+    findresultSrc:"https://ooo.0x0.ooo/2023/11/18/Oe2qHI.png",
+    orangeSrc:"https://ooo.0x0.ooo/2023/11/18/Oe2TzF.png",
+    strawberrySrc:"https://ooo.0x0.ooo/2023/11/18/Oe2UJ6.png",
+    pearSrc:"https://ooo.0x0.ooo/2023/11/18/Oe25XD.png",
+    monkeypeachSrc:"https://ooo.0x0.ooo/2023/11/18/Oe2i7s.png",
+    typeboxSrc:"https://ooo.0x0.ooo/2023/11/18/Oe2WlP.png",
+    sugarboxSrc:"https://ooo.0x0.ooo/2023/11/18/Oe2kTb.png",
+    storagetimeSrc:"https://ooo.0x0.ooo/2023/11/18/Oe2xZl.png",
+    storagemethodSrc:"https://ooo.0x0.ooo/2023/11/18/Oe2zcg.png",
+    compareSrc:"https://ooo.0x0.ooo/2023/11/21/Orz98I.png",
+    searchText: '' ,
+    animationData: {} ,
+  },
+  onLoad(options) {
+    this.setData({
+      searchText:options.fruitname
+    })
+    console.log(options.fruitname)
+    this.animation = wx.createAnimation({
+      duration: 600, // 动画时长，单位ms
+      timingFunction: 'ease', // 动画效果
+   });
+  },
+  handleInput: function(event) {
+    // 更新 searchText 数据
+    this.setData({
+      searchText: event.detail.value
+    });
+  },
+  handleSearch: function() {
+    // 当点击搜索按钮时执行
+    const searchText = this.data.searchText;
+    if (searchText) {
+      // 使用 encodeURIComponent 对 searchText 进行编码
+      const encodedSearchText = encodeURIComponent(searchText);
+      // 打开新页面，跳转到百度搜索结果
+      wx.navigateTo({
+        url: `/pages/webview/webview?url=https://www.baidu.com/s?wd=${encodedSearchText}`
+      });
+    }
+  },
+  toggleNightMode: function(){
+    this.animation.translateY(-110).step(); // -30px为滑动的距离，可以根据需要调整
+    // 更新动画数据
+    this.setData({
+        animationData: this.animation.export()
+    });
+    // 将动画恢复到原位置，准备下一次动画
+    setTimeout(() => {
+    // 音乐播放与暂停逻辑
+    if (app.globalData.isNightMode) {
+      this.setData({ isNightMode: false });
+       app.globalData.isNightMode = false;
+     } else {
+      this.setData({ isNightMode: true });
+      app.globalData.isNightMode = true;
+     }
+      this.animation.translateY(0).step({ duration: 800 });
+      this.setData({
+        animationData: this.animation.export()
+      });
+    }, 500); // 500ms是动画的时长
+  },
+  redirectToMainpage: function() {
+    wx.navigateBack({
+      delta: 1
+    })
+  },
+  redirectToCompare: function() {
+    wx.navigateTo({
+      url: '/pages/compare/compare',
+    })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    this.setData({
+      isNightMode : app.globalData.isNightMode
+  });
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
